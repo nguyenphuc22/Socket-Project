5d60d6148d7122765eba4f9d97bd6cca66f77db8
tuyenphi <76439260+tuyenphi@users.noreply.github.com>
2020-12-22 11:47:24 +0700

    update request_signup

--- Project CNPM/Model/ResquestSignupStruct.cs
+++ Project CNPM/Model/ResquestSignupStruct.cs
@@ -2,11 +2,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Text;
-
+//I'LL DO THIS TASK LATER ^^!
 namespace Project_CNPM.Model
 {
     class ResquestSignupStruct : ChatStruct
     {
+        string userName;
+        string passWord;
+        public ResquestSignupStruct(string userName, string passWord)
+        {
+            this.userName = userName;
+            this.passWord = passWord;
+        }
+
         public override byte[] pack()
         {
             throw new NotImplementedException();

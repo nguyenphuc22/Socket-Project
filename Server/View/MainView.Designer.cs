e3b2a1611bb558d96ef72260f4549eb3562848bb
DI4BL00D <51332386+DI4BL00D@users.noreply.github.com>
2020-12-24 03:53:26 +0700

    Update Server UI

--- Server/View/MainView.Designer.cs
+++ Server/View/MainView.Designer.cs
@@ -29,13 +29,87 @@ namespace Server
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Name = new System.Windows.Forms.ColumnHeader();
+            this.Status = new System.Windows.Forms.ColumnHeader();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.Message = new System.Windows.Forms.ColumnHeader();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(749, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "OFF";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Name,
+            this.Status});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(27, 102);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(371, 336);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // Name
+            // 
+            this.Name.Text = "Name";
+            this.Name.Width = 217;
+            // 
+            // Status
+            // 
+            this.Status.Text = "Status";
+            this.Status.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Status.Width = 150;
+            // 
+            // listView2
+            // 
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Message});
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(405, 103);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(370, 334);
+            this.listView2.TabIndex = 2;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            // 
+            // Message
+            // 
+            this.Message.Text = "Message";
+            this.Message.Width = 350;
+            // 
+            // MainView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            //this.Name = "MainView";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Name;
+        private System.Windows.Forms.ColumnHeader Status;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ColumnHeader Message;
     }
 }
 
--- Server/View/MainView.cs
+++ Server/View/MainView.cs
@@ -18,5 +18,102 @@ namespace Server
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text != "ON")
+            {
+                button1.Text = "ON";
+                Load_Listview();
+                Load_Log();
+            }
+            else
+            {
+                button1.Text = "OFF";
+                Clear_Listview();
+                Clear_Log();
+            }
+        }
+
+        private void Load_Listview()
+        {
+            demo_user user_1 = new demo_user()
+            {
+                Name = "abc",
+                Status = "online"
+            };
+
+            demo_user user_2 = new demo_user()
+            {
+                Name = "def",
+                Status = "online"
+            };
+
+            string[] string_user_1 = { user_1.Name, user_1.Status };
+            var listViewItemUser_1 = new ListViewItem(string_user_1);
+            listView1.Items.Add(listViewItemUser_1);
+
+            string[] string_user_2 = { user_2.Name, user_2.Status };
+            var listViewItemUser_2 = new ListViewItem(string_user_1);
+            listView1.Items.Add(listViewItemUser_2);
+        }
+
+        private void Clear_Listview()
+        {
+            listView1.Items.Clear();
+        }
+
+        private void Load_Log()
+        {
+            demo_message message_1 = new demo_message()
+            {
+                Type = "login",
+                Name = "abc",
+                Message = "has login",
+            };
+            demo_message message_2 = new demo_message()
+            {
+                Type = "chat",
+                Name = "abc",
+                Message = "sent a message to",
+                Reciever = "def"
+            };
+
+            demo_message[] List = { message_1, message_2 };
+            
+            for (int i = 0; i < List.Length; i++)
+            {
+                if (List[i].Type == "login") 
+                {
+                    string message = List[i].Name + " " + List[i].Message;
+                    var ListLogMessage = new ListViewItem(message);
+                    listView2.Items.Add(ListLogMessage);
+                }
+                else
+                {
+                    string message = List[i].Name + " " + List[i].Message + " " + List[i].Reciever;
+                    var ListLogMessage = new ListViewItem(message);
+                    listView2.Items.Add(ListLogMessage);
+                }
+            }
+        }
+
+        private void Clear_Log()
+        {
+            listView2.Items.Clear();
+        }
     }
+}
+
+public class demo_user
+{
+    public string Name;
+    public string Status;
+}
+
+public class demo_message
+{
+    public string Type;
+    public string Name;
+    public string Message;
+    public string Reciever;
 }

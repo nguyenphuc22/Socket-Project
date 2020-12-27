087190bbac3a7666d45fdaaa4620208b4238877b
DI4BL00D <51332386+DI4BL00D@users.noreply.github.com>
2020-12-27 17:44:19 +0700

    Update Client's MainView v0.1

--- Project CNPM/View/MainView.cs
+++ Project CNPM/View/MainView.cs
@@ -18,11 +18,80 @@ namespace Project_CNPM
             InitializeComponent();
         }
 
-
         private void Form1_Load(object sender, EventArgs e)
         {
-           
         }
 
+        public string s_msg;
+
+        public void open_panel1()
+        {
+            this.panel1.Size = new Size(this.panel1.Width, 303);
+        }
+
+        public void close_panel1()
+        {
+            this.panel1.Size = new Size(this.panel1.Width, 72);
+        }
+
+        public void open_panel2()
+        {
+            this.panel2.Size = new Size(this.panel2.Width, 303);
+        }
+
+        public void close_panel2()
+        {
+            this.panel2.Size = new Size(this.panel2.Width, 72);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(this.panel1.Size.Height == 72)
+            {
+                open_panel1();
+                button1.Text = "▼";
+            }
+            else
+            {
+                close_panel1();
+                button1.Text = "≡";
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            close_panel1();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            close_panel1();
+            s_msg = textBox1.Text;
+        }
+
+        private void close_panel1_leave(object sender, EventArgs e)
+        {
+            close_panel1();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (this.panel2.Size.Height == 72)
+            {
+                open_panel2();
+                button7.Text = "▼";
+            }
+            else
+            {
+                close_panel2();
+                button7.Text = "≡";
+            }
+        }
     }
 }
+

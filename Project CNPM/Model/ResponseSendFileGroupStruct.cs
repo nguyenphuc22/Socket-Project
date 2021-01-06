3b79349b6f52fea600dcb97b159c8c11a87c7b33
nguyenphuc22 <nguyenphuc220100@Gmail.com>
2021-01-06 16:17:49 +0700

    ...

--- Project CNPM/Model/ResponseSendFileGroupStruct.cs
+++ Project CNPM/Model/ResponseSendFileGroupStruct.cs
@@ -14,7 +14,7 @@ namespace Project_CNPM.Model
         byte[] fileSize;
         public string getRecGroupName()
         {
-            return "Group:" + this.recGroupName;
+            return "Group:" +    this.recGroupName;
         }
 
         public string getFileName()

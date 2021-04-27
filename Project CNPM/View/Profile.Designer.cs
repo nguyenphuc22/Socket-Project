25942ae5ee9e8c0b263018c61c9a1385d2136bca
nguyenphuc22 <nguyenphuc220100@Gmail.com>
2021-04-27 10:01:09 +0700

    chang ui profile
    chang ui profile
    
    Co-Authored-By: nguyenphuc22 <59957741+nguyenphuc22@users.noreply.github.com>

--- Project CNPM/View/Profile.Designer.cs
+++ Project CNPM/View/Profile.Designer.cs
@@ -32,7 +32,6 @@ namespace Project_CNPM.View
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.changeUsr = new System.Windows.Forms.TextBox();
             this.changePhone = new System.Windows.Forms.TextBox();
-            this.changePicture = new System.Windows.Forms.Button();
             this.changeMail = new System.Windows.Forms.TextBox();
             this.changeMailButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -46,9 +45,12 @@ namespace Project_CNPM.View
             this.pictureBox1.Size = new System.Drawing.Size(136, 136);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // changeUsr
             // 
+            this.changeUsr.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(33)))), ((int)(((byte)(43)))));
+            this.changeUsr.ForeColor = System.Drawing.SystemColors.Window;
             this.changeUsr.Location = new System.Drawing.Point(61, 252);
             this.changeUsr.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.changeUsr.Name = "changeUsr";
@@ -58,6 +60,8 @@ namespace Project_CNPM.View
             // 
             // changePhone
             // 
+            this.changePhone.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(33)))), ((int)(((byte)(43)))));
+            this.changePhone.ForeColor = System.Drawing.SystemColors.Window;
             this.changePhone.Location = new System.Drawing.Point(61, 312);
             this.changePhone.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.changePhone.Name = "changePhone";
@@ -65,19 +69,10 @@ namespace Project_CNPM.View
             this.changePhone.TabIndex = 2;
             this.changePhone.TextChanged += new System.EventHandler(this.changePhone_TextChanged);
             // 
-            // changePicture
-            // 
-            this.changePicture.Location = new System.Drawing.Point(123, 201);
-            this.changePicture.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.changePicture.Name = "changePicture";
-            this.changePicture.Size = new System.Drawing.Size(86, 31);
-            this.changePicture.TabIndex = 5;
-            this.changePicture.Text = "button1";
-            this.changePicture.UseVisualStyleBackColor = true;
-            this.changePicture.Click += new System.EventHandler(this.changePicture_Click);
-            // 
             // changeMail
             // 
+            this.changeMail.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(33)))), ((int)(((byte)(43)))));
+            this.changeMail.ForeColor = System.Drawing.SystemColors.Window;
             this.changeMail.Location = new System.Drawing.Point(61, 367);
             this.changeMail.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.changeMail.Name = "changeMail";
@@ -86,12 +81,14 @@ namespace Project_CNPM.View
             // 
             // changeMailButton
             // 
+            this.changeMailButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.changeMailButton.ForeColor = System.Drawing.SystemColors.HighlightText;
             this.changeMailButton.Location = new System.Drawing.Point(123, 402);
             this.changeMailButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.changeMailButton.Name = "changeMailButton";
             this.changeMailButton.Size = new System.Drawing.Size(79, 31);
             this.changeMailButton.TabIndex = 7;
-            this.changeMailButton.Text = "button2";
+            this.changeMailButton.Text = "Update";
             this.changeMailButton.UseVisualStyleBackColor = true;
             this.changeMailButton.Click += new System.EventHandler(this.changeMailButton_Click_1);
             // 
@@ -99,16 +96,17 @@ namespace Project_CNPM.View
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(33)))), ((int)(((byte)(43)))));
             this.ClientSize = new System.Drawing.Size(345, 439);
             this.Controls.Add(this.changeMailButton);
             this.Controls.Add(this.changeMail);
-            this.Controls.Add(this.changePicture);
             this.Controls.Add(this.changePhone);
             this.Controls.Add(this.changeUsr);
             this.Controls.Add(this.pictureBox1);
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.MaximizeBox = false;
             this.Name = "Profile";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -122,7 +120,6 @@ namespace Project_CNPM.View
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.TextBox changeUsr;
         private System.Windows.Forms.TextBox changePhone;
-        private System.Windows.Forms.Button changePicture;
         private System.Windows.Forms.TextBox changeMail;
         private System.Windows.Forms.Button changeMailButton;
     }
